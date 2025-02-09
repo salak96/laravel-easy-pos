5d54a752acd4a718ce8368cbc59bb5a1db5d0b5b
Arshid <arshid@Arshids-MacBook-Air.local>
2025-02-09 20:08:09 +0530

    init

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Customer;
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
@@ -17,10 +17,11 @@ class ProductSeeder extends Seeder
         $faker = Faker::create();
 
         for ($i = 0; $i < 5; $i++) {
-            Customer::create([
+            Product::create([
                 'name' => $faker->word,
                 'description' => $faker->sentence,
                 'image' => '',
+                'quantity' => 50,
                 'barcode' => $faker->unique()->ean13,  
                 'regular_price' => $faker->randomFloat(2, 50, 200),
                 'price' => $faker->randomFloat(2, 40, 190),
