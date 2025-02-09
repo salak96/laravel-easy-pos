c39004764e02ce7a47b351b298c1e44f39c7962c
Arshid <arshid@Arshids-MacBook-Air.local>
2025-02-09 20:02:35 +0530

    init

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(SettingsSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(ProductSeeder::class);
         $this->call(CustomersSeeder::class);
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class ProductSeeder extends Seeder
 {
@@ -12,6 +14,18 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 5; $i++) {
+            Customer::create([
+                'name' => $faker->word,
+                'description' => $faker->sentence,
+                'image' => '',
+                'barcode' => $faker->unique()->ean13,  
+                'regular_price' => $faker->randomFloat(2, 50, 200),
+                'price' => $faker->randomFloat(2, 40, 190),
+                'status' => true
+            ]);
+        }
     }
 }
